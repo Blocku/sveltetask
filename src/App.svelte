47533afe26669b8da5cd8de36ca8256c01a7fb90
nob <noblockjs@gmail.com>
2025-09-29 21:04:56 +0500

    third

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import Task from './components/Task.svelte'
 
     let isMenuOpened:boolean = $state(true)
-    let toggleMenu:Function = () => isMenuOpened = !isMenuOpened 
+
     let tasks = $state<any>([]) 
     let newTask = $state<any>([{name: "", description: ""}])
 
-    const storage = localStorage.getItem('tasks')
-    if(storage){
-        tasks = JSON.parse(storage)
-    }
+    // Loading the task list from the cache, if there is one
+    onMount(() => {
+        const storage = localStorage.getItem('tasks')
+        if(storage){
+            tasks = JSON.parse(storage)
+        }
+    })
+    
 
     function createTask(e: Event){
         e.preventDefault()
@@ -21,12 +26,12 @@
                 date: (new Date().toDateString())
             }
             tasks = [...tasks, task]
-            toggleMenu()
+            isMenuOpened = false
             newTask = [{name: "", description: ""}]
             localStorage.setItem('tasks', JSON.stringify([...tasks]))
         }
     }
-
+    
     function deleteTask(id: number){
         const newTasks = tasks.filter((task: any) => {
             if(task.id !== id){
@@ -36,15 +41,20 @@
         tasks = newTasks
         localStorage.setItem('tasks', JSON.stringify(newTasks))
     }
-    
+
 </script>
 
 <main class="w-full p-3 space-y-20" >
-
-<button onclick={toggleMenu()} class="w-full rounded-md p-1.5 text-lg text-white font-semibold bg-indigo-500 cursor-pointer" >Add a new text</button>    
+    
+    <!-- Button to open menu -->
+    <button onclick={() => isMenuOpened = !isMenuOpened} class="w-full rounded-md p-1.5 text-lg text-white font-semibold bg-indigo-500 cursor-pointer" >Add a new text</button>    
+    
     {#if isMenuOpened}
         <section class="w-full h-full absolute top-0 left-0" >
-            <button aria-label='close menu' onclick={toggleMenu()} class="w-full h-full absolute bg-black/30" ></button>
+            <!-- Black background for closing the menu -->
+            <button aria-label='close menu' onclick={() => isMenuOpened = !isMenuOpened} class="w-full h-full absolute bg-black/30" ></button>
+
+            <!-- Menu -->
             <div class="h-max absolute m-auto left-5 right-5 top-0 bottom-0 p-5 rounded bg-white" >
                 <div class="flex flex-col gap-y-5" >
                     <div class="space-y-1" >
@@ -61,14 +71,13 @@
         </section>
     {/if}
 
-    
-
+    <!-- List of tasks -->
     <section>
         <ul class="w-full space-y-3" >
             {#each tasks as task}
                 <Task {task} {deleteTask} />
             {:else}
-                <div class="text-center text-lg" >You don't have tasks...</div>
+                <div class="text-center text-lg" >You have no tasks...</div>
             {/each}
         </ul>
     </section>
